7b6b1c6c7fb4b1c81042d59861a2bd88404968c8
mouloud <mouloud619@gmail.com>
2024-02-10 12:33:31 -0600

    more commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faFilm } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faFilm);
 
 
 ReactDOM.render(
